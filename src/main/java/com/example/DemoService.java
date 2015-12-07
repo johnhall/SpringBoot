16a7baeed9ac4cc9db10d1c3a72ada99fc53ce49
John Hall <ojhsolutions@gmail.com>
2015-12-07 16:15:36 -0700

    DEMO-10: initial check-in

---
+++ src/main/java/com/example/DemoService.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * @author John Hall Date: 12/7/15
+ */
+public class DemoService {
+
+    public static String saySomething() {
+        return "Hello World!";
+    }
+
+}
--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,11 +1,10 @@
 package com.example;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
@@ -15,10 +14,5 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-
-    @Test
-    public void testHello() {
-        Assert.assertEquals("Hello World!", DemoApplication.saySomethingThatMeetsCodingStandards());
-    }
 
 }
